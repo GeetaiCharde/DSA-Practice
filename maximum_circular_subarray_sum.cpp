280e3edebabfe3a09b9572416bb8b387e55e1265
Geetai Charde <95483405+GeetaiCharde@users.noreply.github.com>
2022-08-18 12:39:23 +0530

    Add files via upload

---
+++ maximum_circular_subarray_sum.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int kadane (int arr[], int n)
+{
+    int currsum=0;
+    int maxSum = INT_MIN;
+    for(int i=0; i<n; i++){
+        currsum += arr[i];
+        if(currsum <0){
+            currsum=0;
+        }
+        maxSum= max(maxSum, currsum);
+    }
+    return maxSum;
+}
+
+int main()
+{
+    int n;
+    cout<<"Array Size: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Array element: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int wrapsum;
+    int nonwrapsum;
+
+    nonwrapsum = kadane(arr,n);
+    int totalsum=0;
+    for(int i=0; i<n; i++){
+        totalsum += arr[i];
+        arr[i] = -arr[i];
+    }
+    wrapsum = totalsum + kadane(arr,n);
+    cout<<max(wrapsum,nonwrapsum)<<endl;
+
+    return 0;
+
+}
