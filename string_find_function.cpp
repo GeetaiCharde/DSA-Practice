692952ebf9be8d2e7bc5d2bc61afeaf209538208
Geetai Charde <95483405+GeetaiCharde@users.noreply.github.com>
2022-08-26 00:12:11 +0530

    Add files via upload

---
+++ string_find_function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+// substring is a small part of a string
+int main()
+{
+    string s1="GoodLuck";
+
+    cout<<s1.find("Go")<<endl; // this function return the 1st index where it is found or the starting index of substring.
+    return 0; 
+
+}
