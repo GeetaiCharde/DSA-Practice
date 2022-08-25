26cfd59bdba97fd12b42c448c787cc7443729e78
Geetai Charde <95483405+GeetaiCharde@users.noreply.github.com>
2022-08-25 19:23:17 +0530

    Add files via upload

---
+++ 3_string/string_clear_function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str = "asjdjsdjkd dirconkjrhwern ijlnocuinore";
+
+    str.clear();
+    cout<<str<<endl;
+    return 0;
+}
